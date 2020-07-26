da023875442a9d070d4d715d5b1690f793ed24cc
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-07-26 18:27:39 +0800

    Integrate stripe test integration and added favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_wrI8AWWxodHJGfb01Bs1QPvA00aQRQK66p';
+
+  const onToken = (token) => {
+    // Pass to backend
+    console.log(token);
+
+    alert('Payment successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
