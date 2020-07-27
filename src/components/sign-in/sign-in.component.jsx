afe6c66472fab959f5c59e7bc0ec275effbd3dbc
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-07-27 17:37:00 +0800

    Integrate styled components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       email: '',
@@ -21,6 +21,7 @@ class SignIn extends React.Component {
     event.preventDefault();
 
     const { email, password } = this.state;
+
     try {
       await auth.signInWithEmailAndPassword(email, password);
       this.setState({ email: '', password: '' });
@@ -37,37 +38,31 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className='sign-in' onSubmit={this.handleSubmit}>
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <FormInput
             name='email'
-            value={this.state.email}
-            required
-            label='email'
             type='email'
             handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
           />
           <FormInput
             name='password'
-            value={this.state.password}
-            required
-            label='password'
-            handleChange={this.handleChange}
             type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
           />
-
           <div className='buttons'>
-            <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton
-              type='button'
-              onClick={signInWithGoogle}
-              isGoogleSignIn
-            >
-              {' '}
-              Sign in with Google{' '}
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -46,6 +46,9 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () => {
+  console.log('signing in via google');
+  auth.signInWithPopup(provider);
+};
 
 export default firebase;
